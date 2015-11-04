bbf4ac15c8a35e9daf3272d9136c185788cb5417
John Beech <john.beech@bbc.co.uk>
2015-11-04 17:09:35 +0000

    Added test for destructuring column headings

--- test/dax/destructureReport-spec.js
+++ test/dax/destructureReport-spec.js
@@ -4,9 +4,19 @@ var destructureReport = require('../../lib/dax/destructureReport');
 var sampleDaxReport = require('../fixtures/sampleDaxReport.json');
 
 describe('Destructure Report', function() {
-	it('should correctly extract the title from the DAX report', function() {
-		var report = destructureReport(sampleDaxReport);
 
-		expect(report.title).to.deep.equal('Experimental Support - Brightness - pass');
-	})
+	var report;
+
+	beforeEach(function() {
+		report = destructureReport(sampleDaxReport);
+	});
+
+	it('should correctly extract the title from the DAX report', function() {
+		expect(report.title).to.equal('Experimental Support - Brightness - pass');
+	});
+
+	it('should correctly extract the column headings from the DAX report', function() {
+		var expectedColumnHeadings = ['app_version', 'brand', 'model', 'Action type', 'Action name', 'pass', 'Browsers', 'Hidden events'];
+		expect(report.columnHeadings).to.deep.equal(expectedColumnHeadings);
+	});
 });
