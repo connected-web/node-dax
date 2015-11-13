44a1ab4aa3ecf0ff2164a441dc80b07eddc5762f
John Beech <john.beech@bbc.co.uk>
2015-11-13 11:55:09 +0000

    Add test for file that does not need updating

--- test/util/testFileAge-spec.js
+++ test/util/testFileAge-spec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var testFileAge = require('../../lib/util/testFileAge');
+var fs = require('fs');
 
 describe('Test File Age', function() {
     it('should report file not found, for non-existing files', function(done) {
@@ -8,7 +9,7 @@ describe('Test File Age', function() {
             message: 'File not found',
             recommendation: 'Update the file'
         };
-        testFileAge('Non existing file', 0).then(function(result) {
+        testFileAge('non-existing-file.txt', 0).then(function(result) {
             try {
                 expect(result).to.deep.equal(expected);
                 done();
@@ -16,5 +17,26 @@ describe('Test File Age', function() {
                 done(ex);
             }
         }).catch(done)
+    });
+
+    it('should report file does not need updating, for file younger then limit', function(done) {
+        var expected = {
+            ageInMs: 0,
+            message: 'File is younger than 1000ms limit',
+            recommendation: 'No update required'
+        };
+        var filePath = __dirname + '/../../daxCache/testFileAge.json';
+        fs.writeFileSync(filePath, JSON.stringify({
+            some: 'data'
+        }), 'utf8');
+        testFileAge(filePath, 1000).then(done).catch(function(result) {
+            try {
+                expect(result).to.have.property('message', expected.message);
+                expect(result).to.have.property('recommendation', expected.recommendation);
+                done();
+            } catch (ex) {
+                done(ex);
+            }
+        });
     });
 });
